23ccbdea9c381ce18b47467c24b35286037fa9d4
Uteshev Maxim <guitarheroe2012@ya.ru>
2023-03-09 21:28:59 +0300

    fixed errors

--- Regular expression.py
+++ Regular expression.py
@@ -13,7 +13,7 @@ substitution_l_f_s_name = r'\1, \2, \3,'
 substitution_organization = r' \1'
 substitution_numbers = r' +7(\2)\3-\4-\5'
 substitution_numbers_optional = r' доб.\2, '
-for rows in contacts_list[1:]:
+for rows in contacts_list:
     join_list = ",".join(rows)
     l_f_s_name = re.sub(pattern_l_f_s_name, substitution_l_f_s_name, join_list, re.A)
     organization = re.sub(pattern_organization, substitution_organization, l_f_s_name, re.A)
@@ -21,6 +21,7 @@ for rows in contacts_list[1:]:
     numbers_optional = re.sub(pattern_numbers_optional, substitution_numbers_optional, numbers, re.A)
     split_list = numbers_optional.split(',')
     all_data.append(split_list)
+# print(all_data)
 repeated_dict = {}
 repeated_data = []
 v_sorted_list_1 = []
@@ -38,21 +39,26 @@ final_list = []
 zip_list = []
 values_list = []
 for record in all_data:
-    repeated_dict[f'{record[0]}, {record[1]}'] = repeated_dict.setdefault(f'{record[0]}, {record[1]}', 0) + 1
-    if repeated_dict.setdefault(f'{record[0]}, {record[1]}') != 1:
-        repeated_data.append(f'{record[0]}, {record[1]}')
+    record_0 = ','.join(record[:2])
+    record_2 = ','.join(record[2:])
+    repeated_dict[record_0] = repeated_dict.setdefault(record_0, 0) + 1
+    if repeated_dict.setdefault(record_0) != 1:
+        repeated_data.append(record_0)
 for record in all_data:
-    data_dct = {f'{record[0]}, {record[1]}': record[2:]}
+    record_0 = ','.join(record[:2])
+    record_2 = ','.join(record[2:])
+    # print(record_0,record_2)
+    data_dct = {record_0: record_2}
     for keys, values in data_dct.items():
         if keys in repeated_data[0]:
             for index, value in enumerate(values):
-                if value != '' and value != ' ':
+                # if value != '' and value != ' ':
                     i_v_1 = index, value
                     if i_v_1 not in i_v_list_1:
                         i_v_list_1.append(i_v_1)
         if keys in repeated_data[1]:
             for index, value in enumerate(values):
-                if value != '' and value != ' ':
+                # if value != '' and value != ' ':
                     i_v_2 = index, value
                     if i_v_2 not in i_v_list_2:
                         i_v_list_2.append(i_v_2)
@@ -67,9 +73,10 @@ for n in index_list_1:
 for key, value in i_v_dict_2.items():
     if value not in v_sorted_list_1:
         v_sorted_list_1.append(value)
+sorted_str_1 = ','.join(v_sorted_list_1)
 for key, value in value_dict.items():
     if key in repeated_data[0]:
-        value_dict[key] = v_sorted_list_1
+        value_dict[key] = sorted_str_1
 for i, v in i_v_list_2:
     index_list_2.append(i)
     index_list_2.sort()
@@ -80,10 +87,12 @@ for n in index_list_2:
 for key, value in i_v_dict_4.items():
     if value not in v_sorted_list_2:
         v_sorted_list_2.append(value)
+sorted_str_2 = ','.join(v_sorted_list_2)
 for key, value in value_dict.items():
     if key in repeated_data[1]:
-        value_dict[key] = v_sorted_list_2
+        value_dict[key] = sorted_str_2
 pairs = list(value_dict.items())
+print(pairs)
 with open("phonebook.csv", 'w') as f:
     datawriter = csv.writer(f, delimiter=",")
     for rows in pairs:
